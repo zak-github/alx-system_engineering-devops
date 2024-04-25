f6b96a68b0bbb775d1edcf4e7ff154104580ec6c
zak <elmyar.zakaria@gmail.com>
2024-04-25 12:30:05 +0000

    d

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,17 +3,17 @@
 script that queries subscribers on a given Reddit subreddit.
 """
 
+
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return f"existing subreddit\nOK\n"
     else:
-        return 0
+        return f"nonexisting subreddit\nOK\n"
+
