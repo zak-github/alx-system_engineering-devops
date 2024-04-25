436e5997a5d415f1a485821f28aaa42f8e7aa296
zak <elmyar.zakaria@gmail.com>
2024-04-25 13:04:56 +0000

    fff

---
+++ 0x16-api_advanced/0-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+import sys
+
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        result = number_of_subscribers(sys.argv[1])
+        print(result)
+
